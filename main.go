9c5ad7937d4c0c5bd6be967209dc72e449fa9533
muly <msrinivasareddy@gmail.com>
2018-04-30 17:07:20 -0700

    reset my repo code with coding challenge original code

--- main.go
+++ main.go
@@ -1,55 +1,32 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 	_ "github.com/lib/pq"
+
+	"github.com/rackerlabs/GoCodingChallenge/todo"
 )
 
-var db *sql.DB
-
-func initDb() error {
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	sslmode := "disable"
-	host := "localhost"
-	port := "5433" //TODO: change this back to 5432
-
-	dburl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", user, password, host, port, dbname, sslmode)
-
-	var err error
-	db, err = sql.Open("postgres", dburl)
-	if err != nil {
-		return err
-	}
-
-	err = db.Ping()
-	if err != nil {
-		return err
-	}
-
-	log.Println("DB connected")
-	return nil
+// Status :=
+func Status(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Println("Status Request Received")
+	w.WriteHeader(200)
+	fmt.Fprint(w, "OK\n")
 }
 
 func main() {
-	err := initDb()
-	if err != nil {
-		log.Fatalf("db init failed: %s", err.Error())
-		return
-	}
+	router := httprouter.New()
+	router.GET("/", Status)
+	router.POST("/todos", todo.Create)
+	router.GET("/todos", todo.List)
 
-	port := "8080"
+	log.Println("Starting server...")
 
-	router := mux.NewRouter()
-	router.HandleFunc("/todo/{id}", editTodoHandler).Methods("PUT")
-
-	log.Printf("app running on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	// Make sure you have DB_USER, DB_PASSWORD and DB_NAME environment variables set.
+	// We use them elsewhere
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
